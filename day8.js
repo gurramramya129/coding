5fe24a9bc4c369a219bec5902d5d12b18e27e701
ramyagurram454129 <ramyagurram20@gmail.com>
2023-08-24 11:39:34 +0530

    commiting pagination component

---
+++ day8.js
@@ -0,0 +1,73 @@
+import React from 'react'
+ import Data from './Data.json'
+ import 'bootstrap/dist/css/bootstrap.min.css'
+ import { useState } from 'react'
+
+ function App() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const recordsPerpage = 5;
+  const lastIndex = currentPage * recordsPerpage;
+  const  firstIndex = lastIndex - recordsPerpage;
+  const records = Data.slice(firstIndex, lastIndex)
+  const npage= Math.ceil(Data.length / recordsPerpage)
+  const numbers = [...Array(npage + 1).keys()].slice(1)
+
+  return (
+    <div>
+    <table className='table'>
+    <thead>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    </thead>
+    <tbody>
+    {records.map((d, i) => (
+      <tr key={i}>
+      <td>{d.ID}</td>
+      <td>{d.Name}</td>
+      <td>{d.email}</td>
+      </tr> 
+    ))}
+    </tbody>
+    </table>
+    <nav>
+    <ul className='pagination'>
+    <li className='page-item'>
+    <a href='#' className='page-link'
+    onClick={prePage}>Prev</a>
+    </li>
+    {
+      numbers.map((n, i) => (
+        <li className={`page-item ${currentPage === n ? 'active' : ' ' }`} key={i}>
+        <a href='#' className='page-link'
+        onClick={changeCPage} >{n}</a>
+        </li>
+      ))
+      }
+      <li className='page-item'>
+      <a href='#' className='page-link'
+      onClick={nextPage}>Next</a>
+      </li>
+      </ul>
+      </nav>
+    </div>
+  )
+ function prePage() {
+if(currentPage !== 1) {
+  setCurrentPage(currentPage - 1)
+}
+ }
+
+ function changeCPage(id) {
+  setCurrentPage(id)
+ }
+
+ function  nextPage() {
+  if(currentPage !== npage) {
+    setCurrentPage(currentPage + 1)
+  }
+ }
+  
+}
+
+export default App
