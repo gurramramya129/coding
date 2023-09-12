403179827fc950ccca92eafeb3684b0971d717bb
ramyagurram454129 <ramyagurram20@gmail.com>
2023-09-12 18:09:17 +0530

    commiting dropdown

---
+++ Day14.js
@@ -0,0 +1,25 @@
+// script.js
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
+
+function setAriaExpandedFalse() {
+  dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
+}
+
+function closeDropdownMenu() {
+  dropdown.forEach((drop) => {
+    drop.classList.remove("active");
+    drop.addEventListener("click", (e) => e.stopPropagation());
+  });
+}
+
+function toggleHamburger() {
+    navMenu.classList.toggle("show");
+    hamburgerBtn.setAttribute(
+        "aria-expanded",
+        hamburgerBtn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+    );
+}
